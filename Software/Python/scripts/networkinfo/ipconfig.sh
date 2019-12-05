369cefeb30c994f2d197aa9794e8c511b4d6bd4f
Jiri Brejcha <jirka@jiribrejcha.net>
2019-12-05 10:04:58 +0000

    Improvements to the info displayed in ipconfig menu, added domain name info, added a 1s delay to allow the Pi to get an address from DHCP server

--- Software/Python/scripts/networkinfo/ipconfig.sh
+++ Software/Python/scripts/networkinfo/ipconfig.sh
@@ -3,11 +3,12 @@
 #Displays IP address, subnet mask, default gateway, DNS servers, speed, duplex, DHCP server IP address and name 
 ACTIVEIP=$(ip a | grep "eth0" | grep "inet" | cut -d '/' -f1 | cut -d ' ' -f6)
 SUBNET=$(ip a | grep "eth0" | grep "inet" | cut -d ' ' -f6 | tail -c 4)
-LEASEDIP=$(grep "fixed-address" /var/lib/dhcp/dhclient.eth0.leases | tail -1 | cut -d ' ' -f4 | cut -d ';' -f1)
+LEASEDIPISUSED=$(grep "$ACTIVEIP" /var/lib/dhcp/dhclient.leases)
 ETH0ISUP=$(/sbin/ifconfig eth0 | grep "RUNNING")
 DHCPENABLED=$(grep -i "eth0" /etc/network/interfaces | grep "dhcp" | grep -v "#")
-DHCPSRVNAME=$(grep "server-name" /var/lib/dhcp/dhclient.eth0.leases | tail -1 | cut -d '"' -f2)
-DHCPSRVADDR=$(grep "dhcp-server-identifier" /var/lib/dhcp/dhclient.eth0.leases | tail -1 | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
+DHCPSRVNAME=$(grep -A 13 'interface "eth0"' /var/lib/dhcp/dhclient.leases | tail -13 | grep -B 1 -A 10 "$ACTIVEIP" | grep "server-name" | cut -d '"' -f2)
+DHCPSRVADDR=$(grep -A 13 'interface "eth0"' /var/lib/dhcp/dhclient.leases | tail -13 | grep -B 1 -A 10 "$ACTIVEIP" | grep "dhcp-server-identifier" | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
+DOMAINNAME=$(grep -A 13 'interface "eth0"' /var/lib/dhcp/dhclient.leases | tail -13 | grep -B 1 -A 10 "$ACTIVEIP" | grep "domain-name " | cut -d '"' -f2)
 DEFAULTGW=$(/sbin/route -n | grep G | grep eth0 | cut -d ' ' -f 10)
 SPEED=$(sudo ethtool eth0 | grep -q "Link detected: yes" && sudo ethtool eth0 | grep "Speed" | sed 's/....$//' | cut -d ' ' -f2  || echo "Disconnected")
 DUPLEX=$(sudo ethtool eth0 | grep -q "Link detected: yes" && sudo ethtool eth0 | grep "Duplex" | cut -d ' ' -f 2 || echo "Disconnected")
@@ -29,9 +30,16 @@ if [ "$ETH0ISUP" ]; then
     done
 
     #DHCP server info
-    if [[ "$ACTIVEIP" = "$LEASEDIP" ]] && [[ "$DHCPENABLED" ]]; then
-        echo "DHCP srv: $DHCPSRVNAME"
-        echo "DHCP srv: $DHCPSRVADDR"
+    if [[ "$LEASEDIPISUSED" ]] && [[ "$DHCPENABLED" ]]; then
+        if [[ "$DHCPSRVNAME" ]] && [[ "$LEASEDIPISUSED" ]]; then
+            echo "DHCP server name: $DHCPSRVNAME"
+        fi
+        if [[ "$DHCPSRVADDR" ]] && [[ "$LEASEDIPISUSED" ]]; then
+            echo "DHCP server address: $DHCPSRVADDR"
+        fi
+        if [[ "$DOMAINNAME" ]] && [[ "$LEASEDIPISUSED" ]]; then
+            echo "Domain: $DOMAINNAME"
+        fi
     else
         echo "No DHCP server used"
     fi
